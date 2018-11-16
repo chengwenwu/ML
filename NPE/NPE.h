7f0bf2c635eeef8e69a48b49784ec8af59773225
wuchengwen <2681595858@qq.com>
2018-11-17 00:41:02 +0800

    add the chapter four code non-parameter-estimate

---
+++ NPE/NPE.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "storage.h"
+class NPE
+{
+public:
+	NPE();
+	~NPE();
+
+	/*
+	*设置样本文件，该样本应该具有固定格式，详细会在实验报告中说明
+	*/
+	void setSampleFile(string fileName_in);
+
+	/*
+	*设置有多少个类别
+	*/
+	void setClassSize(int classSize_in);
+
+	/*
+	*设置样本数据中向量的长度
+	*向量一般是指列向量
+	*/
+	void setVectorLen(int vecLen_in);
+
+	/*
+	*设置样本大小
+	*/
+	void setSampleSize(int sampleSize_in);
+
+	/*
+	@vec_str:用户输入将要判别的向量,各个元素之间用逗号隔开
+	*输出parzen窗判别结果
+	*/
+	int judgePar(string vec_str_in, float h);
+
+	/*
+	@vec_str:用户输入的x,各个元素之间用逗号隔开
+	*输出KNN概率
+	*/
+	float* knnProbability(string vec_str_in,int k);
+
+	
+
+private:
+	Storage storage;//负责样本文件的读取
+	int classSize;//表示有多少个类别
+	int vecLen;//样本中向量的长度
+	int sampleSize;//每个类别中样本容量应该相同
+
+	/*
+	*用户输入处理
+	*将用户输入形如[1,2,3,4,5]的向量转化为一个Matrix对象输出
+	*/
+	Matrix userInputProcess(string vec_str_in);
+};
---
+++ NPE/storage.cpp
@@ -0,0 +1,58 @@
+#include "storage.h"
+Storage::Storage()
+{
+	this->vectorLen = -1;
+}
+
+Storage::~Storage()
+{
+	if (this->infile)
+		this->infile.close();
+}
+/*
+*打开文件
+*/
+void Storage::setfile(string fileName)
+{
+	if (this->infile)
+	{
+		infile.close();
+	}
+	this->infile.open(fileName);
+	if (!this->infile)
+	{
+		cout << "File open error!\n";
+		system("pause");
+	}
+}
+
+/*
+*设置样本数据中向量的长度
+*/
+void  Storage::setVectorLen(int vectorLen_in)
+{
+	this->vectorLen = vectorLen_in;
+	this->matrix = Matrix(this->vectorLen, 1);
+}
+
+/*
+*只支持顺序读取
+*/
+Matrix& Storage::readData()
+{
+	float data;
+	for(int counter = 1; counter <= this->vectorLen && this->infile >> data; counter++)
+	{
+		//cout << data;
+		this->matrix.setElement(counter, 1, data);
+	}
+	return this->matrix;
+}
+
+/*
+*让文件指针回到文件开始的地方
+*/
+void Storage::reset()
+{
+	this->infile.seekg(0, ios::beg);
+}
